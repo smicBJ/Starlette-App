f5a265d87faf2ebf5f4079abc78500d23f4f8ca0
Jonathan Fernandes <jhfernan@mail.usf.edu>
2023-09-07 14:35:50 +0800

    Created main with very simple server example

---
+++ main.py
@@ -0,0 +1,16 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
+import uvicorn
+
+async def homepage(request):
+    return JSONResponse({'hello': 'world'})
+
+
+app = Starlette(debug=True, routes=[
+    Route('/', homepage),
+])
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
